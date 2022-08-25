2b55b0472739becc5578475332b96d22189d95fd
Altynai <urmatovnaa05@gmail.com>
2022-08-25 11:06:55 +0600

    russian name for apps

--- admin_panel_app/apps.py
+++ admin_panel_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AdminPanelAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'admin_panel_app'
+    verbose_name = 'Админ Панель'
--- title_app/apps.py
+++ title_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TitleAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'title_app'
+    verbose_name = 'Тайтл'
