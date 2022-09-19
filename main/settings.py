e5793d8adf3aea5e01111cdbdf722cad46013b51
Altynai <urmatovnaa05@gmail.com>
2022-09-20 00:14:13 +0600

    add default permission class

--- main/settings.py
+++ main/settings.py
@@ -156,6 +156,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     # 'DEFAULT_AUTHENTICATION_CLASSES': [
     #     'rest_framework.authentication.TokenAuthentication',
     # ],
