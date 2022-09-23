8d950ff7a9682c0059b88d9b717568be53d4faef
Altynai <urmatovnaa05@gmail.com>
2022-09-23 21:28:14 +0600

    add count system

--- manga_report/tasks.py
+++ manga_report/tasks.py
@@ -3,61 +3,38 @@ from openpyxl import load_workbook
 from .models import ReportManga
 
 
-@shared_task
-def add(x, y):
-    print("Начался расчёт")
-    print("Завершился расчёт")
-    return x + y
-
-
 # takes excel file and makes report into, then returns changed file
 @shared_task
 def make_report(report_object_id):
-    print('hello world')
     report_object = ReportManga.objects.get(id=report_object_id)
     input_file = report_object.input_file
     excel_file = load_workbook(input_file.path)
     page = excel_file["Лист1"]
 
-    lst = page["A"]
+    lst = page["B"]
     res = {}
-    for cell in lst:  # A2
+    for cell in lst:  # B2
         row_number = cell.row  # 2
 
         if row_number == 1:
             continue
+        type = cell.value
 
-        name = cell.value
-        if name not in res:
-            res[name] = {}
-
-        date_cell = page[f"B{row_number}"].value  # 01.01.2022
-
-        if date_cell not in res[name]:
-            res[name][date_cell] = 1
+        if type not in res:
+            res[type] = 1
         else:
-            res[name][date_cell] += 1
+            res[type] += 1
 
     # header
     row_number = 1
-    page[f"E{row_number}"] = "seller name"
-    letters = "FGHIJKLMNOPQR"  # ...
+    page[f"E{row_number}"] = "type"
+    page[f"F{row_number}"] = "title's count"
     i = 0
-    for date_value in res[name]:
-        # print(date_value)
-        page[f"{letters[i]}{row_number}"] = date_value
+    numbers = "23456789"
+    for type_value in res.keys():
+        page[f"E{numbers[i]}"] = type_value
+        page[f"F{numbers[i]}"] = res[type_value]
         i += 1
-
-    # cells
-    for name, date_qty_dict in res.items():
-        row_number += 1
-
-        page[f"E{row_number}"] = name
-
-        i = 0
-        for date_value, qty in date_qty_dict.items():
-            page[f"{letters[i]}{row_number}"] = qty
-            i += 1
 
     output_file_path = input_file.path.replace(".xlsx", "_ready.xlsx")
     excel_file.save(output_file_path)
